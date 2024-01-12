c425395bddc278872a6736c9f26469dbdb0c6a34
Hous1423 <kh_talbi@esi.dz>
2024-01-12 16:25:10 +0100

    mise a jour de la note vehicule/marque par admin

--- app/Controllers/AdminAvisController.php
+++ app/Controllers/AdminAvisController.php
@@ -3,7 +3,8 @@
 require_once ("../app/Models/AvisMarqueModel.php");
 require_once ("../app/Models/AvisVehiculeModel.php");
 require_once ("../app/Models/UserModel.php");
-
+require_once ("../app/Models/MarqueModel.php");
+require_once ("../app/Models/VehiculeModel.php");
 
 //VIEWS
 require_once("../app/Views/HeadView.php");
@@ -20,6 +21,8 @@ class AdminAvisController extends Controller{
             //models declaration area
             $avisMarques = new AvisMarqueModel();
             $avisVehicules = new AvisVehiculeModel();
+            $marques = new MarqueModel();
+            $vehicules = new VehiculeModel();
 
             if(isset($data[1])){
                 switch ($data[1]){
@@ -28,17 +31,31 @@ class AdminAvisController extends Controller{
                         $avis = $avisMarques->get($data[2]);
                         $avis['valide'] = 1;
                         $avisMarques->update($avis);
+                        $marque = $marques->get($avis['marque_id']);
+                        $marque['note'] = $avisMarques->calculateNote($avis['marque_id']);
+                        $marques->update($marque);
                         break;
                     case 'RefuserMarque' :
                         $avisMarques->delete($data[2]);
+                        $avis = $avisMarques->get($data[2]);
+                        $marque = $marques->get($avis['marque_id']);
+                        $marque['note'] = $avisMarques->calculateNote($avis['marque_id']);
+                        $marques->update($marque);
                         break;
                     case 'ValiderVehicule' :
                         $avis = $avisVehicules->get($data[2]);
                         $avis['valide'] = 1;
                         $avisVehicules->update($avis);
+                        $vehicule = $vehicules->get($avis['vehicule_id']);
+                        $vehicule['note'] = $avisVehicules->calculateNote($avis['vehicule_id']);
+                        $vehicules->update($vehicule);
                         break;
                     case 'RefuserVehicule' :
                         $avisVehicules->delete($data[2]);
+                        $avis = $avisMarques->get($data[2]);
+                        $vehicule = $vehicules->get($avis['vehicule_id']);
+                        $vehicule['note'] = $avisVehicules->calculateNote($avis['vehicule_id']);
+                        $vehicules->update($vehicule);
                         break;
 
                 }
